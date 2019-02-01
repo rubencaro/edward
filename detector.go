e36bf59502aa4aa87cdab74db45d219b50fd5424
Rubén Caro <ruben.caro.estevez@gmail.com>
2019-02-01 17:31:47 +0100

    Add ShouldFire

--- detector.go
+++ detector.go
@@ -1,1 +1,7 @@
 package main
+
+// ShouldFire returns true when the difference between
+// given hashes is bigger than given threshold
+func ShouldFire(hash1, hash2 uint64, threshold uint64) bool {
+	return Distance(hash1, hash2) <= threshold
+}
